fd757d125695328627856b69443a183ca9c7e63a
Dmytro Orlov <orlov.dm@gmail.com>
2021-05-10 23:44:42 +0200

    2021-05-10-23-44-42

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -1,10 +1,25 @@
 package com.example
 
 import zio._
+import zio.actors.Actor.Stateful
+import zio.actors._
 import zio.console._
+
+import java.io.File
+
+sealed trait Command[+_]
+
+case class DoubleCommand(value: Int) extends Command[Int]
 
 object Main extends App {
   def run(args: List[String]) = {
+    val stateful = new Stateful[Any, Unit, Command] {
+      def receive[A](state: Unit, msg: Command[A], context: Context): UIO[(Unit, A)] =
+        msg match {
+          case DoubleCommand(value) => UIO(((), value * 2))
+        }
+    }
+
     val program =
       for {
         _ <- putStrLn("123")
